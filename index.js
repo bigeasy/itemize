776d4586390acfccc388ef8269116229e16e6290
Alan Gutierrez <alan@prettyrobots.com>
2016-05-30 22:33:21 -0500

    Sketch of parser.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-module.exports = 1
+function Parser () {
+}
+
+Parser.prototype._name = function (rest) {
+    $ = /^(\s*)([\w'"<])(.*)$/.exec(string)
+    switch ($[2]) {
+    case '"':
+    case "'":
+    case '<':
+        this._string(string)
+        break
+    default:
+        this._naked(string)
+        break
+    }
+}
+
+Parser.prototype._value = function (string) {
+    var $
+
+    $ = /^(\s*)(\S)(.*)$/.exec(string)
+    switch ($[2]) {
+    case '"':
+    case "'":
+    case '<':
+        this._string(string)
+        break
+    case '[':
+        this._array(string)
+        break
+    case '{':
+    case '(':
+        this._object($[1], string)
+        break
+    default:
+        this._naked(string)
+        break
+    }
+}
+
+module.exports = Parser
